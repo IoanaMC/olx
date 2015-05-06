846a7aa088774bc833a94ac54a97d4b61fc74918
IoanaMC <yoanna90ro@yahoo.com>
2015-05-07 01:19:58 +0300

    Setup sass.

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -7,21 +7,20 @@
     <title>Anunturi Vanzari</title>
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
-    <link href="css/custom-responsive.css" rel="stylesheet">
     <link href="css/font-awesome.css" rel="stylesheet">
-	<link rel="stylesheet" href="css/flexslider.css" type="text/css">
+	<link href="css/flexslider.css" rel="stylesheet">
 
     <link href="css/reset.css" rel="stylesheet">
     <link href="css/general.css" rel="stylesheet">
     <link href="css/base.css" rel="stylesheet">
     <link href="css/buttons.css" rel="stylesheet">
-    <link href="css/contact.css" rel="stylesheet">
     <link href="css/login.css" rel="stylesheet">
     <link href="css/homepage.css" rel="stylesheet">
     <link href="css/create-account.css" rel="stylesheet">
-    <link href="css/add-product.css" rel="stylesheet">
+    <link href="css/add-news.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
